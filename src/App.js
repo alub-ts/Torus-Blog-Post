e078de18c9ce90a9b5d39945aec41f24bc638bd8
alvin lubetkin <alvin@terminal.co>
2019-11-20 10:41:33 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,67 @@
-import { Web3Versions } from "@terminal-packages/sdk";
+import {
+  TerminalHttpProvider,
+  Web3Versions,
+  EnvironmentTypes
+} from "@terminal-packages/sdk";
 import Torus from "@toruslabs/torus-embed";
 import React from "react";
 import "./App.css";
-const Web3 = require("web3");
-const sdk = require("@terminal-packages/sdk");
+import Web3 from "web3";
+
+const apiKey = "zYwDsCwqeD6Sg9plihI/xQ==";
+const projectId = "ZoanBDVJvKDlLYAM";
+
+const devAPI = "zYwDsCwqeD6Sg9plihI/xQ==";
+const devProjectID = "ZoanBDVJvKDlLYAM";
+
+const stgAPI = "keLUuNmCTjqKJgAOf7CbIw==";
+const stgProjectID = "aMDxVPWGpmQjBYgN";
+let web3;
 function App() {
   const torus = new Torus();
   const setup = async function() {
     await torus.init();
     await torus.login();
-  };
-  const tester = () => {
-    const web3 = new Web3(
-      new sdk.TerminalHttpProvider({
+    // const tester = async () => {
+    web3 = new Web3(
+      new TerminalHttpProvider({
         customHttpProvider: torus.provider,
-        apiKey: "Z0CsA9B5xAkCjfw0kcKh6g==",
+        apiKey: devAPI,
         source: "TORUS",
-        environment: sdk.EnvironmentTypes.live,
-        projectId: "bYzPZdjZezVQKvLA",
-        web3Version: Web3Versions.one
+        environment: EnvironmentTypes.dev,
+        projectId: devProjectID,
+        web3Version: Web3Versions.one,
+        networkSource: "matic"
       })
     );
-    //const web3 = new Web3(torus.provider);
+  };
+  //const web3 = new Web3(torus.provider);
+  const test = () => {
     web3.eth.getBalance(
-      "0xE22FD0840d127E44557D5E19A0A9a52EAfc3e297",
-      (error, result) => {
-        console.log(error);
-        console.log(result);
-      }
-    );
-
-    web3.eth.getTransaction(
-      "0xe627d0bc7348e0c0a8a662ffefdc5b420ac669a9805811a61884ac3845910738",
+      "0xaD7d7543188e13b63699eEF2f0B963d6d589B47D",
       (error, result) => {
         console.log(error);
         console.log(result);
       }
     );
   };
+  const sendTx = () => {
+    web3.eth.getAccounts((err, res) => {
+      console.log(res[0]);
+      // console.log(err);
+      web3.eth.sendTransaction({
+        from: res[0],
+        to: "0xaD7d7543188e13b63699eEF2f0B963d6d589B47D",
+        value: web3.utils.toWei("0.0001")
+      });
+    });
+  };
+
   setup();
   return (
     <div className="App">
-      <button onClick={() => tester()}>TEST</button>
+      <button onClick={() => test()}>TEST</button>
+      <button onClick={() => sendTx()}>SendTx</button>
     </div>
   );
 }
